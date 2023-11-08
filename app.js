e46143d4af5030c9b4f6bd295d4129a736db97c3
jacklyno <110430029+jacklyno@users.noreply.github.com>
2023-11-08 11:26:31 -0800

    Update app.js

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const previewImage = document.getElementById('previewImage');
 const cancelButton = document.getElementById('cancelButton');
 const submitButton = document.getElementById('submitButton');
 
-const dbName = 'indexedbd1';
+const dbName = 'indexeddb1';
 const objectStoreName = 'images';
 
 function formatDateToISO(timestamp) {
@@ -25,7 +25,8 @@ function initIndexedDB(callback) {
     request.onupgradeneeded = function (event) {
         const db = event.target.result;
         if (!db.objectStoreNames.contains(objectStoreName)) {
-            db.createObjectStore(objectStoreName, { keyPath: 'timestamp' });
+            const objectStore = db.createObjectStore(objectStoreName, { keyPath: 'timestamp' });
+            objectStore.createIndex('timestamp', 'timestamp', { unique: true });
         }
     };
 
@@ -52,35 +53,6 @@ function saveImageToIndexedDB(db, file) {
 
     const reader = new FileReader();
     reader.onload = function () {
-        const imageBlob = new Blob([new Uint8Array(reader.result)], { type: file.type });
+        const imageBlob = new Blob([reader.result], { type: file.type }); // Use Blob to store image data
 
-        store.put({ timestamp: formattedTimestamp, file: imageBlob });
-
-        transaction.oncomplete = function () {
-            console.log('Image saved to IndexedDB with timestamp key:', formattedTimestamp);
-        };
-
-        transaction.onerror = function (event) {
-            console.error('Error saving image to IndexedDB:', event.target.error);
-        };
-    };
-    reader.readAsArrayBuffer(file);
-}
-
-takePictureButton.addEventListener('click', () => {
-    imageInput.click();
-});
-
-imageInput.addEventListener('change', (e) => {
-    const file = e.target.files[0];
-    if (file) {
-        const imageURL = URL.createObjectURL(file);
-        previewImage.src = imageURL;
-    }
-});
-
-cancelButton.addEventListener('click', () => {
-    previewImage.src = '';
-    imageInput.value = '';
-});
-
+        store.put({ timestamp: formattedTimestamp,
