07443e555b04477d92c8abe5768900f2ec434fb4
jacklyno <110430029+jacklyno@users.noreply.github.com>
2023-11-08 12:54:00 -0800

    Update app.js

--- app.js
+++ app.js
@@ -3,53 +3,67 @@ const imageInput = document.getElementById('imageInput');
 const previewImage = document.getElementById('previewImage');
 const cancelButton = document.getElementById('cancelButton');
 const submitButton = document.getElementById('submitButton');
-const storeName = 'images';
-const dbName = 'indexeddb1'; // Define the dbName variable
 
-// ...
+const dbName = 'indexedbd1';
+const objectStoreName = 'images';
+
+// Function to initialize IndexedDB and create the object store
+function initIndexedDB(callback) {
+    const request = indexedDB.open(dbName, 1);
+
+    request.onupgradeneeded = function (event) {
+        const db = event.target.result;
+        if (!db.objectStoreNames.contains(objectStoreName)) {
+            db.createObjectStore(objectStoreName, { keyPath: 'timestamp' });
+        }
+    };
+
+    request.onsuccess = function (event) {
+        const db = event.target.result;
+        callback(db);
+    };
+}
+
+// Function to save the image file to IndexedDB with a timestamp key
+function saveImageToIndexedDB(db, file) {
+    const transaction = db.transaction(objectStoreName, 'readwrite');
+    const store = transaction.objectStore(objectStoreName);
+    const timestamp = new Date().getTime(); // Get the current timestamp
+    store.put({ timestamp, file });
+
+    transaction.oncomplete = function () {
+        console.log('Image saved to IndexedDB with timestamp key:', timestamp);
+        db.close();
+    };
+}
+
+takePictureButton.addEventListener('click', () => {
+    imageInput.click();
+});
+
+imageInput.addEventListener('change', (e) => {
+    const file = e.target.files[0];
+    if (file) {
+        const imageURL = URL.createObjectURL(file);
+        previewImage.src = imageURL;
+    }
+});
+
+// Add event listeners for the "Cancel" and "Submit" buttons
+cancelButton.addEventListener('click', () => {
+    // Clear the preview
+    previewImage.src = '';
+    // Remove the image file from memory
+    imageInput.value = '';
+});
 
 submitButton.addEventListener('click', () => {
+    // Add your logic to handle image submission, if needed
     if (imageInput.files.length > 0) {
         const file = imageInput.files[0];
         initIndexedDB((db) => {
             saveImageToIndexedDB(db, file);
         });
     }
-});
-
-function saveImageToIndexedDB(db, file) {
-    const transaction = db.transaction(storeName, 'readwrite');
-    const store = transaction.objectStore(storeName);
-    const timestamp = new Date().getTime();
-    const formattedTimestamp = formatDateToISO(timestamp);
-
-    const reader = new FileReader();
-    reader.onload = function () {
-        const arrayBuffer = reader.result;
-
-        transaction.oncomplete = function () {
-            const request = store.add({ timestamp: formattedTimestamp, image: arrayBuffer });
-
-            request.onsuccess = function () {
-                console.log('Image saved to IndexedDB with timestamp key:', formattedTimestamp);
-                clearGalleryImages();
-                renderAvailableImagesFromDb();
-                renderStorageQuotaInfo();
-            };
-
-            request.onerror = function (event) {
-                console.error('Error adding image to IndexedDB:', event.target.error);
-            };
-        };
-
-        reader.readAsArrayBuffer(file);
-    };
-}
-
-// ...
-
-cancelButton.addEventListener('click', () => {
-    previewImage.src = '';
-    imageInput.value = '';
 });
 
