809510a73a03e02fdadc1056f4418b03d0b0285e
jacklyno <110430029+jacklyno@users.noreply.github.com>
2023-11-08 17:57:44 -0800

    Update app.js

--- app.js
+++ app.js
@@ -4,23 +4,6 @@ const previewImage = document.getElementById('previewImage');
 const cancelButton = document.getElementById('cancelButton');
 const submitButton = document.getElementById('submitButton');
 
-// Function to initialize IndexedDB and create the object store
-function initIndexedDB(callback) {
-    const request = indexedDB.open(dbName, 1);
-
-    request.onupgradeneeded = function (event) {
-        const db = event.target.result;
-        if (!db.objectStoreNames.contains(objectStoreName)) {
-            db.createObjectStore(objectStoreName, { keyPath: 'timestamp' });
-        }
-    };
-
-    request.onsuccess = function (event) {
-        const db = event.target.result;
-        callback(db);
-    };
-}
-
 // Function to save the image file to IndexedDB with a timestamp key
 function saveImageToIndexedDB(db, file) {
     const transaction = db.transaction(objectStoreName, 'readwrite');
@@ -30,46 +13,55 @@ function saveImageToIndexedDB(db, file) {
 
     transaction.oncomplete = function () {
         console.log('Image saved to IndexedDB with timestamp key:', timestamp);
-        db.close();
     };
 }
 
-// Add event listeners for the "Cancel" and "Submit" buttons
+// Event listeners for specific buttons
+takePictureButton.addEventListener('click', () => {
+    imageInput.click();
+});
 
+imageInput.addEventListener('change', (e) => {
+    const file = e.target.files[0];
+    if (file) {
+        const imageURL = URL.createObjectURL(file);
+        previewImage.src = imageURL;
+    }
+});
+
+cancelButton.addEventListener('click', () => {
+    // Clear the preview
+    previewImage.src = '';
+    // Remove the image file from memory
+    imageInput.value = '';
+});
+
+submitButton.addEventListener('click', () => {
+    // Add your logic to handle image submission, if needed
+    if (imageInput.files.length > 0) {
+        const file = imageInput.files[0];
+        saveImageToIndexedDB(db, file);
+        // Clear the preview
+        previewImage.src = '';
+        // Remove the image file from memory
+        imageInput.value = '';
+    }
+});
+
+// Event listener for page load
 window.addEventListener('load', async () => {
-    // Initialize IndexedDB when the page loads
-    initIndexedDB((db) => {
-        // Now, set up event listeners and other logic involving IndexedDB
-        takePictureButton.addEventListener('click', () => {
-            imageInput.click();
-        });
+    const request = indexedDB.open(dbName, 1);
 
-        imageInput.addEventListener('change', (e) => {
-            const file = e.target.files[0];
-            if (file) {
-                const imageURL = URL.createObjectURL(file);
-                previewImage.src = imageURL;
-            }
-        });
+    request.onupgradeneeded = (event) => {
+        const db = event.target.result;
+        db.createObjectStore(objectStoreName, { keyPath: 'timestamp' });
+    };
 
-        // Add event listeners for the "Cancel" and "Submit" buttons
-        cancelButton.addEventListener('click', () => {
-            // Clear the preview
-            previewImage.src = '';
-            // Remove the image file from memory
-            imageInput.value = '';
-        });
+    request.onsuccess = (event) => {
+        db = event.target.result;
+    };
 
-        submitButton.addEventListener('click', () => {
-            // Add your logic to handle image submission, if needed
-            if (imageInput.files.length > 0) {
-                const file = imageInput.files[0];
-                saveImageToIndexedDB(db, file);
-                // Clear the preview
-                previewImage.src = '';
-                // Remove the image file from memory
-                imageInput.value = '';
-            }
-        });
-    });
+    request.onerror = (event) => {
+        console.error('Error opening IndexedDB:', event.target.error);
+    };
 });
